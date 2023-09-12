651e65073d34d18e0d2dfdd3fcc16f6f309e4f22
joeoakes <jxo19@psu.edu>
2023-09-11 21:41:59 -0400

    updated

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,11 +1,14 @@
 public class VendingMachine {
-    private String[][] products; //Multi-dimensional array to store product information
+    //Define the class level Multi-dimensional array to store product information
+    private String[][] products;
 
+    //Constructor Method accepts the number of rows and COls
     public VendingMachine(int rows, int cols) {
+        // Declare and dimension a Multi-dimensional array of Strings
         products = new String[rows][cols];
     }
 
-    // Method to add a product to the vending machine
+    // Method to add a product to the vending machine element/slot in the Multi-dimensional array
     public void addProduct(int row, int col, String productName) {
         if (row >= 0 && row < products.length && col >= 0 && col < products[0].length) {
             products[row][col] = productName;
@@ -30,6 +33,7 @@ public class VendingMachine {
     }
 
     // Method to display the current state of the vending machine
+    //Uses the nested for loop construct used to work with a Multi-dimensional array
     public void display() {
         for (int i = 0; i < products.length; i++) {
             for (int j = 0; j < products[0].length; j++) {
@@ -51,19 +55,25 @@ public class VendingMachine {
         //vendingmachine is the object, addProduct() is the method
         vendingMachine.addProduct(0, 0, "Soda");  //row 0, col 0 
         vendingMachine.addProduct(0, 1, "Chips");
+        vendingMachine.addProduct(0, 2, "Gum");
+        vendingMachine.addProduct(1, 0, "poprocks");
         vendingMachine.addProduct(1, 1, "Candy");
+        vendingMachine.addProduct(1, 2, "Gum");
         vendingMachine.addProduct(2, 2, "Water");
 
+        //Show all the contents in the Vending Machine
         System.out.println("Vending Machine Contents:");
         vendingMachine.display();
 
+        //Get some of the products from the Vending Machine product inventory
         System.out.println("\nGetting products from the vending machine:");
-        System.out.println("1. " + vendingMachine.getProduct(0, 0));
-        System.out.println("2. " + vendingMachine.getProduct(0, 1));
-        System.out.println("3. " + vendingMachine.getProduct(1, 1));
-        System.out.println("4. " + vendingMachine.getProduct(2, 2));
+        System.out.println("1. " + vendingMachine.getProduct(0, 0));  //Get the soda
+        System.out.println("2. " + vendingMachine.getProduct(0, 1));  //get the chips
+        System.out.println("3. " + vendingMachine.getProduct(1, 1));  //get the candy
+        System.out.println("4. " + vendingMachine.getProduct(2, 2));  //get the water
 
+        //Display what is left in inventory
         System.out.println("\nUpdated Vending Machine Contents:");
-        vendingMachine.display();
+        vendingMachine.display();  //Only Gum and poprocks are the products left
     }
 }
